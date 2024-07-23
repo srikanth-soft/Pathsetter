187a1450ecac6d2b59a93c326370c40bb3ed557e
MujeebMudassir <mujeebmujeeb2071@gmail.com>
2024-07-23 13:03:05 +0530

    products responsive issue

--- src/Screens/Home/Products.jsx
+++ src/Screens/Home/Products.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-
+import React, { useState, useEffect, useRef } from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const codeString2 = `const Products = () => {
   const [selectedProduct, setSelectedProduct] = useState('product1');
@@ -24,15 +23,15 @@ function CodeSnippet({ codeString }) {
 }
 
 const Products = () => {
-  const [selectedProduct, setSelectedProduct] = useState('product1');
-  const [prevProduct, setPrevProduct] = useState('product1');
+  const [selectedProduct, setSelectedProduct] = useState("product1");
+  const [prevProduct, setPrevProduct] = useState("product1");
   const [isVisible, setIsVisible] = useState(false);
   const containerRef = useRef(null);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
-        entries.forEach(entry => {
+        entries.forEach((entry) => {
           if (entry.isIntersecting) {
             setIsVisible(true);
             observer.unobserve(entry.target); // Stop observing after visibility is set
@@ -52,7 +51,7 @@ const Products = () => {
       }
     };
   }, []);
-  
+
   const handleProductSelect = (productId) => {
     setPrevProduct(selectedProduct);
     setSelectedProduct(productId);
@@ -60,14 +59,14 @@ const Products = () => {
 
   const getTransitionClass = (productId) => {
     if (selectedProduct === productId) {
-      return '';
+      return "";
     }
 
-    const direction = selectedProduct > prevProduct ? 'left' : 'right';
+    const direction = selectedProduct > prevProduct ? "left" : "right";
     return `transform transition-transform ${
       selectedProduct === productId
         ? `translate-x-0 `
-        : direction === 'left'
+        : direction === "left"
         ? `-translate-x-full`
         : `translate-x-full`
     }`;
@@ -79,18 +78,17 @@ const Products = () => {
       <div className="flex justify-center gap-5 mb-5">
         <button
           className={`text-black text-nowrap rounded-full py-2 px-5 transition ${
-            selectedProduct === 'product1' ? 'bg-white' : ' text-white'
+            selectedProduct === "product1" ? "bg-white" : " text-white"
           }`}
-          onClick={() => handleProductSelect('product1')}
+          onClick={() => handleProductSelect("product1")}
         >
-          
           Product 1
         </button>
         <button
           className={`text-black text-nowrap rounded-full py-2 px-5 transition ${
-            selectedProduct === 'product2' ? 'bg-white' : ' text-white'
+            selectedProduct === "product2" ? "bg-white" : " text-white"
           }`}
-          onClick={() => handleProductSelect('product2')}
+          onClick={() => handleProductSelect("product2")}
         >
           Product 2
         </button>
@@ -104,41 +102,49 @@ const Products = () => {
         </button> */}
       </div>
 
-      <div  className={`relative  min-h-[600px] md:min-h-[400px]  overflow-hidden `}>
+      <div
+        className={`relative  min-h-[700px] md:min-h-[400px]  overflow-hidden `}
+      >
         <div
-        ref={containerRef}
-          className={`absolute top-0 w-full ${getTransitionClass('product1')}`}
-          style={{ transitionDuration: '500ms' }}
+          ref={containerRef}
+          className={`absolute  top-0 w-full ${getTransitionClass("product1")}`}
+          style={{ transitionDuration: "500ms" }}
         >
-          <div className={`grid grid-cols-1 md:grid-cols-2 gap-5 text-white    `}>
-            <div className={`flex flex-col justify-between items-start ${isVisible?"right-in":""}`}>
+          <div
+            className={`grid grid-cols-1 md:grid-cols-2 gap-5 text-white    `}
+          >
+            <div
+              className={`flex flex-col justify-between items-start opacity-0 ${
+                isVisible ? "right-in" : ""
+              }`}
+            >
               <p className="text-2xl mb-5">Product 1</p>
-              <p className='text-justify'>
+              <p className="text-justify">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Consequuntur nesciunt unde facilis? Inventore nam, quos ut quae
                 ipsam similique aspernatur, distinctio corporis nostrum fuga
                 pariatur voluptatibus nihil ipsa! Consectetur, architecto. Lorem
                 ipsum dolor sit amet consectetur adipisicing elit. Dolor
-                necessitatibus omnis consequuntur officia earum illum sequi amet,
-                quam quas nemo corrupti saepe architecto tenetur ducimus ab illo
-                distinctio magnam reprehenderit.
+                necessitatibus omnis consequuntur officia earum illum sequi
+                amet, quam quas nemo corrupti saepe architecto tenetur ducimus
+                ab illo distinctio magnam reprehenderit.
               </p>
               <button>Learn more &gt;</button>
             </div>
-            <div className={`${isVisible?"left-in":""}`}>
+            <div className={`opacity-0 ${isVisible ? "left-in" : ""}`}>
               <CodeSnippet codeString={codeString2} />
             </div>
           </div>
         </div>
         <div
-          className={`absolute top-0 w-full ${getTransitionClass('product2')}`}
-          style={{ transitionDuration: '500ms' }}
+          className={`absolute top-0 w-full ${getTransitionClass("product2")}`}
+          style={{ transitionDuration: "500ms" }}
         >
           <div className="grid grid-cols-1 md:grid-cols-2 gap-5 text-white">
             <div>
               <CodeSnippet codeString={codeString2} />
             </div>
-            <div className='flex flex-col justify-between items-start'>
+            <div className="flex flex-col justify-between items-start">
               <p className="text-2xl mb-5">Product 2</p>
               <p className="text-justify">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -146,9 +152,9 @@ const Products = () => {
                 ipsam similique aspernatur, distinctio corporis nostrum fuga
                 pariatur voluptatibus nihil ipsa! Consectetur, architecto. Lorem
                 ipsum dolor sit amet consectetur adipisicing elit. Dolor
-                necessitatibus omnis consequuntur officia earum illum sequi amet,
-                quam quas nemo corrupti saepe architecto tenetur ducimus ab illo
-                distinctio magnam reprehenderit.
+                necessitatibus omnis consequuntur officia earum illum sequi
+                amet, quam quas nemo corrupti saepe architecto tenetur ducimus
+                ab illo distinctio magnam reprehenderit.
               </p>
               <button>Learn more &gt;</button>
             </div>
