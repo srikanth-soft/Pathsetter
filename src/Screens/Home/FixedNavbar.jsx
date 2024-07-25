f66fd61b870fdc3615add5465b039bd4a37724a9
MujeebMudassir <mujeebmujeeb2071@gmail.com>
2024-07-25 15:47:33 +0530

    first commit

--- src/Screens/Home/FixedNavbar.jsx
+++ src/Screens/Home/FixedNavbar.jsx
@@ -17,7 +17,7 @@ const FixedNavbar = () => {
   return (
       <nav className={`bg-black   text-[13px] font-medium text-[#919191] rounded-md p-4 ${scrollY>740?"fixed":"absolute"} transition z-30 top-2 max-lg:right-2`}>
         <button className="headerButton hover:text-white">HOME</button>
-        <Link className="headerButton hover:text-white">About Us</Link>
+        <Link to={"/about"} className="headerButton hover:text-white">About Us</Link>
         <button className="headerButton hover:text-white">Products</button>
         <button className="headerButton hover:text-white">FAQs</button>
         <button className="text-[13px] text-black rounded-sm px-1 py-[1px] font-medium bg-gradient-to-br from-green-400 via-blue-600 to-purple-600 ">CTA</button>
